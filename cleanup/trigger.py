e01f720f50fe1a82b4f1c8cfdad9cd7604682aaa
Jarred Kozlick <jkozlick@gmail.com>
2020-07-08 22:22:47 -0600

    script works

--- cleanup/trigger.py
+++ cleanup/trigger.py
@@ -1,17 +1,49 @@
 import boto3
 import os
 
+keep = 3
 s3_client = boto3.client('s3')
-bucket_name = os.environ.get('BUCKET_NAME')
 
 def lambda_handler(event, context):
+    record = event['Records'][0]['s3']
+    bucket = record['bucket']['name']
+    key = record['object']['key']
     
-    return event
+    response = s3_client.list_object_versions(
+        Bucket=bucket,
+        Prefix=key
+    )
+    
+    version_len = len(response['Versions'])
 
+    if version_len > keep:
+        sorted_versions = sorted(
+            response['Versions'], 
+            key = lambda version: version['LastModified'],
+            reverse = True
+        )
+
+        for a_version in sorted_versions[keep:version_len]:
+            s3_client.delete_object(
+                Bucket=bucket,
+                Key=key,
+                VersionId=a_version['VersionId']
+            )
+        
+        earliest = sorted_versions[keep-1]['LastModified']
+        delete_markers = response.get('DeleteMarkers') or []
+
+        for delete_marker in delete_markers:
+            if delete_marker['LastModified'] <= earliest:
+                s3_client.delete_object(
+                    Bucket=bucket,
+                    Key=key,
+                    VersionId=delete_marker['VersionId']
+            )
 
 if __name__ == '__main__':
     import json
-    with open('event') as file:
+    with open(os.environ['RP_TEST_EVENT']) as file:
         event = json.loads(file.read())
-    print(lambda_handler(event, 'context'))
+    lambda_handler(event, 'context')
 
